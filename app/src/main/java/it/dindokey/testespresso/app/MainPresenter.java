285dd5d373b12c9995e76fd5aae0f6ab6b826bf1
SimoneC <simone.caldon@trovaprezzi.it>
2016-03-09 14:53:27 +0100

    wip: aggiunto rxjava

--- app/src/main/java/it/dindokey/testespresso/app/MainPresenter.java
+++ app/src/main/java/it/dindokey/testespresso/app/MainPresenter.java
@@ -1,9 +1,13 @@
 package it.dindokey.testespresso.app;
 
-import android.os.AsyncTask;
-import it.dindokey.testespresso.app.api.ProductsApi;
-
 import javax.inject.Inject;
+
+import it.dindokey.testespresso.app.api.ProductsApi;
+import rx.Observable;
+import rx.Observer;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by simone on 2/24/16.
@@ -23,19 +27,62 @@ public class MainPresenter
 
     public void onStart()
     {
-        new AsyncTask<Void, Void, String[]>()
+//        new AsyncTask<Void, Void, String[]>()
+//        {
+//            @Override
+//            protected void onPostExecute(String[] result)
+//            {
+//                view.refreshProductList(result);
+//            }
+//
+//            @Override
+//            protected String[] doInBackground(Void... params)
+//            {
+//                return productsApi.getProducts();
+//            }
+//        }.execute();
+
+
+        Observable<String[]> productsObservable = Observable.create(new Observable.OnSubscribe<String[]>()
         {
             @Override
-            protected void onPostExecute(String[] result)
+            public void call(Subscriber<? super String[]> subscriber)
             {
-                view.refreshProductList(result);
+                try
+                {
+                    String[] products = productsApi.getProducts();
+                    subscriber.onNext(products);
+                    subscriber.onCompleted();
+                }
+                catch (Exception e)
+                {
+                    subscriber.onError(e);
+                }
             }
+        });
 
-            @Override
-            protected String[] doInBackground(Void... params)
-            {
-                return productsApi.getProducts();
-            }
-        }.execute();
+        productsObservable
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<String[]>()
+                {
+                    @Override
+                    public void onCompleted()
+                    {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e)
+                    {
+
+                    }
+
+                    @Override
+                    public void onNext(String[] products)
+                    {
+                        view.refreshProductList(products);
+                    }
+                });
     }
 }
