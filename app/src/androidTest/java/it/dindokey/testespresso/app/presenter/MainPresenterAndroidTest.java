75ba8ecc46ed776e1fd48b42fbcc4c098ae077e6
SimoneC <simone.caldon@trovaprezzi.it>
2016-03-21 14:16:10 +0100

    wip: refactoring

--- app/src/androidTest/java/it/dindokey/testespresso/app/presenter/MainPresenterAndroidTest.java
+++ app/src/androidTest/java/it/dindokey/testespresso/app/presenter/MainPresenterAndroidTest.java
@@ -2,7 +2,10 @@ package it.dindokey.testespresso.app.presenter;
 
 import android.os.Bundle;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import it.dindokey.testespresso.app.SchedulerManager;
 import it.dindokey.testespresso.app.api.ProductsApiService;
@@ -10,20 +13,27 @@ import it.dindokey.testespresso.app.model.ProductsModel;
 import rx.schedulers.Schedulers;
 
 import static junit.framework.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
 
 /**
  * Created by simone on 3/17/16.
  */
 public class MainPresenterAndroidTest
 {
+    @Mock
+    ProductsApiService productsApiServiceMock;
+    private MainPresenter presenter;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        MockitoAnnotations.initMocks(this);
+        SchedulerManager schedulerManager = new SchedulerManager(Schedulers.immediate(),Schedulers.immediate());
+        presenter = new MainPresenter(productsApiServiceMock, schedulerManager);
+    }
+
     @Test
     public void save_model_to_instance_state() throws Exception
     {
-        ProductsApiService productsApiServiceMock = mock(ProductsApiService.class);
-        SchedulerManager schedulerManager = new SchedulerManager(Schedulers.immediate(),Schedulers.immediate());
-        MainPresenter presenter = new MainPresenter(productsApiServiceMock, schedulerManager);
-
         ProductsModel testModel = new ProductsModel();
         testModel.setItems(new String[]{"test product"});
         presenter.setProductsModel(testModel);
@@ -33,6 +43,5 @@ public class MainPresenterAndroidTest
 
         ProductsModel expected = outState.getParcelable(MainPresenter.MODEL);
         assertEquals(expected, testModel);
-
     }
 }
